586d14911c6f282f07008d36dee4791a3c21a547
Greg Korenevsky <greg_koren@yahoo.com>
2023-02-28 00:50:43 -0800

    Optimize solution for finding sub-arrays

--- src/main/java/net/prep/screen/tech/array/sum/Solution.java
+++ src/main/java/net/prep/screen/tech/array/sum/Solution.java
@@ -6,62 +6,51 @@ import java.util.Map;
 public class Solution {
 
     /*
-    Note: this solution works for arrays with positive numbers, and mixed positive and negative numbers.
-          It doesn't work for arrays with negative numbers only. One possible modification for the latter would
-          be to use absolute values when storing sums and calculating complements.
+    Note: this solution works for arrays with only positive, only negative, and mixed positive and negative numbers.
      */
 
     int[] getSubArray(int[] a, int sum) {
         int[] result = new int[0];
-        boolean cont = true;
-        int[] pSums = new int[a.length];
         Map<Integer, Integer> pSumMap = new HashMap<>();
+        boolean positive = false;
 
         if (a.length == 0) {
             return result;
         }
 
         // calculate partial sums
-        for (int i = 0, pSum = 0; i < a.length && cont; i++) {
+        for (int i = 0, pSum = 0; i < a.length; i++) {
             // if current cell == sum
             if (a[i] == sum) {
                 // return singleton array
                 result = new int[]{a[i]};
-                cont = false;
+                break;
             } else {
+                // indicate that a positive # is present
+                positive |= (a[i] > 0);
                 // calculate and save partial sum
                 pSum += a[i];
-                pSums[i] = pSum;
                 // if partial sum == sum
                 if (pSum == sum) {
                     // allocate space for subarray
                     result = new int[i + 1];
                     // copy contents to subarray
                     System.arraycopy(a, 0, result, 0, i + 1);
-                    cont = false;
+                    break;
                 } else {
-                    // save partial sum and its corresponding offset
+                    // save current partial sum and its corresponding offset
                     pSumMap.put(pSum, i);
-                }
-            }
-        }
 
-        if (!cont) {
-            // found required sum
-            return result;
-        }
-
-        for (int i = 0; i < a.length && cont; i++) {
-            // ignore partial sums <= sum
-            // (a partial sum cannot be equal to sum, since it would have been found already)
-            if (pSums[i] > sum && pSumMap.containsKey(pSums[i] - sum)) {
-                int j = pSumMap.get(pSums[i] - sum);
-                // ensure start of sub-array precedes its end
-                if (j < i) {
-                    cont = false;
-                    result = new int[i - j];
-                    // copy contents to subarray
-                    System.arraycopy(a, j+1, result, 0, i - j);
+                    // if partial sum exceeds the target sum, check whether there is
+                    // a prior partial sum which can be subtracted from the current one
+                    // to yield the desired result.
+                    if (pSumMap.containsKey(pSum - sum)) {
+                        int j = pSumMap.get(pSum - sum);
+                        result = new int[i - j];
+                        // copy contents to subarray
+                        System.arraycopy(a, j + 1, result, 0, i - j);
+                        break;
+                    }
                 }
             }
         }
--- src/test/java/net/prep/screen/tech/array/sum/SolutionTest.java
+++ src/test/java/net/prep/screen/tech/array/sum/SolutionTest.java
@@ -4,12 +4,16 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+// TODO: check with duplicate partial sums, i.e. what if a partial sum can be obtained > 1 time
+
 class SolutionTest {
 
     private final Solution sut = new Solution();
 
     private final int[] testArray = new int[]{1, 2, 5, 7, 100, 10, 11, 12, 101};
     private final int[] emptyArray = new int[0];
+
+    private final int[] testNegArray = new int[]{-1, -2, -5, -7, -100, -10, -11, -12, -101};
 
     @Test
     public void mustFindInSingletonArray() {
@@ -84,4 +88,48 @@ class SolutionTest {
         assertArrayEquals(new int[]{-12, -101}, sut.getSubArray(input, -113));
     }
 
+    @Test
+    public void mustFindRegularRunningNegSum() {
+        int[] result = sut.getSubArray(testNegArray, -15);
+        assertArrayEquals(new int[]{-1,-2,-5,-7}, result);
+    }
+
+    @Test
+    public void mustFindLastRegularRunningNegSum() {
+        int[] result = sut.getSubArray(testNegArray, -249);
+        assertArrayEquals(testNegArray, result);
+    }
+
+    @Test
+    public void mustFindNegSubArray() {
+        int[] result = sut.getSubArray(testNegArray, -121);
+        assertArrayEquals(new int[]{-100, -10, -11}, result);
+    }
+    @Test
+    public void mustFindAnotherNegSubArray() {
+        int[] result = sut.getSubArray(testNegArray, -112);
+        assertArrayEquals(new int[]{-5, -7, -100}, result);
+    }
+
+    @Test
+    public void mustFindYetOtherNegSubArrays() {
+        int[] result = sut.getSubArray(testNegArray, -122);
+        assertArrayEquals(new int[]{-5, -7, -100, -10}, result);
+
+        result = sut.getSubArray(testNegArray, -14);
+        assertArrayEquals(new int[]{-2, -5, -7}, result);
+
+        result = sut.getSubArray(testNegArray, -113);
+        assertArrayEquals(new int[]{-12, -101}, result);
+    }
+
+    @Test
+    public void testAlternatingNegAndPosNumbers() {
+        var array = new int[]{-1, -2, -5, -7, 100, 10, 11, 12, -101};
+
+        assertArrayEquals(new int[]{-2, -5, -7}, sut.getSubArray(array, -14));
+        assertArrayEquals(new int[]{-7, 100}, sut.getSubArray(array, 93));
+        assertArrayEquals(new int[]{-5, -7, 100, 10}, sut.getSubArray(array, 98));
+        assertArrayEquals(new int[]{11, 12, -101}, sut.getSubArray(array, -78));
+    }
 }
